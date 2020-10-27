f630e22a5ddeebeeaabd5f74845e331a3f2373b5
MhmdRyhn <rayhan.cse12@gmail.com>
2020-10-28 00:22:42 +0600

    Add SignUpSchema and ConfirmSignUpSchema

---
+++ schema/auth.go
@@ -0,0 +1,14 @@
+package schema
+
+
+type SignUpSchema struct {
+	Email string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6,max=32,uppercase,lowercase,digit,punctuation"`
+	ConfirmPassword string `json:"confirmPassword" validate:"required,min=6,max=32,uppercase,lowercase,digit,punctuation"`
+}
+
+
+type ConfirmSignUpSchema struct {
+	Email string `json:"email" validate:"required,email"`
+	ConfirmationCode string `json:"confirmationCode" validate:"required,min=1"`
+}
