7a3f1b2e53c6622a99706a9027bcfdda4a9362a3
MhmdRyhn <rayhan.cse12@gmail.com>
2020-11-09 13:36:48 +0600

    Add authentication middleware

--- route/middleware.go
+++ route/middleware.go
@@ -3,16 +3,35 @@ package route
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/mhmdryhn/cognito-auth-service/core/utils"
 )
 
 
 func AuthenticationMiddeware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
-			"message": "From authentication middleware",
-			"statusCode": http.StatusOK,
-		})
+		accessToken := ctx.Request.Header.Get("Authorization")
+		token := strings.Replace(accessToken, "Bearer ", "", 1)
+		val := utils.JWTValidator{}
+		if claims, err := val.Validate(token); err == nil {
+			ctx.Set(
+				"user", 
+				map[string]interface{} {
+					"username": claims["sub"], 
+					// "group": claims["groups"],
+				},
+			)
+			ctx.Next()
+		} else {
+			ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
+				"data": map[string]string {},
+				"errors": map[string]string {"TokenError": err.Error()},
+				"message": err.Error(),
+				"statusCode": http.StatusUnauthorized,
+			})
+		}
 	}
 }
