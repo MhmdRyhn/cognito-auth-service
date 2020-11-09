5a8d2d2bfcf4f118ae9f99555345aa6f20e21693
MhmdRyhn <rayhan.cse12@gmail.com>
2020-11-09 13:10:07 +0600

    Update jwt validator by using struct

--- core/utils/jwt.go
+++ core/utils/jwt.go
@@ -2,8 +2,8 @@ package utils
 
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
 	"os"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -11,25 +11,28 @@ import (
 )
 
 
-var publicKeysURL string = fmt.Sprintf(
-	"https://cognito-idp.%s.amazonaws.com/%s/.well-known/jwks.json", 
-	os.Getenv("AWS_REGION"),
-	os.Getenv("USER_POOL_ID"),
-	// "1234567",
+var (
+	awsRegion string = os.Getenv("AWS_REGION")
+	userPoolId string = os.Getenv("USER_POOL_ID")
+	appClientId string = os.Getenv("APP_CLIENT_ID")
+	publicKeysURL string = fmt.Sprintf(
+		"https://cognito-idp.%s.amazonaws.com/%s/.well-known/jwks.json", 
+		awsRegion, userPoolId,
+	)
+	issuer string = fmt.Sprintf(
+		"https://cognito-idp.%s.amazonaws.com/%s",
+		awsRegion, userPoolId,
+	)
 )
 
 
-// type JWTValidatorMethod interface {
-// 	func KeyFunction (*jwt.Token) (interface{}, error)
-// }
+type JWTValidator struct {
+	claims jwt.MapClaims
+}
 
 
-// type JWTValidator struct {
-// 	tokenString string
-// }
-
-
-func KeyFunction (token *jwt.Token) (interface{}, error) {
+func (self *JWTValidator) KeyFunction (token *jwt.Token) (interface{}, error) {
+	// Validate signing method/algorithm
 	if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 	}
@@ -41,43 +44,56 @@ func KeyFunction (token *jwt.Token) (interface{}, error) {
 	// Get JWKs (JSON Web Keys)
 	publicKeySet, err := jwk.Fetch(publicKeysURL)
 	if err != nil {
-		return nil, errors.New(fmt.Sprintf("Failed to parse key: %s", err))
+		return nil, errors.New("Failed to parse public key")
 	}
 	// Search `kid` in JWKs
 	keys := publicKeySet.LookupKeyID(kid);
 	if len(keys) == 0 {
-			return nil, fmt.Errorf("key %v not found", kid)
+			return nil, errors.New("Key not found.")
 	}
-
+	// Create token key
 	var tokenKey interface{}
 	if err := keys[0].Raw(&tokenKey); err != nil {
-		return nil, errors.New("Failed to create token key")
+		return nil, errors.New("Failed to create token key.")
 	}
-
 	return tokenKey, nil
 }
 
 
-func ParseJWT(tokenString string) (jwt.MapClaims, error) {
-	token, err := jwt.Parse(tokenString, KeyFunction,)
-
-	if token.Valid {
+func (self *JWTValidator) ParseJWT(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, self.KeyFunction)
+	if token != nil && token.Valid {
 		if claims, ok := token.Claims.(jwt.MapClaims); ok {
+			self.claims = claims
 			return claims, nil
 		}
 		return nil, err
-	// } else if ve, ok := err.(*jwt.ValidationError); ok {
-	// 	// fmt.Println(token)
-	// 	// fmt.Println(ve)
-	// 	if ve.Errors & jwt.ValidationErrorMalformed != 0 {
-	// 		return nil, err
-	// 	} else if ve.Errors & (jwt.ValidationErrorExpired | jwt.ValidationErrorNotValidYet) != 0 {
-	// 		// Token is either expired or not active yet
-	// 		return nil, err
-	// 	} else {
-	// 		return nil, err
-	// 	}
+	} else if ve, ok := err.(*jwt.ValidationError); ok {
+		if ve.Errors & jwt.ValidationErrorMalformed != 0 {
+			return nil, errors.New("That's not even a token. Please make sure you are using a valid token.")
+		}
 	} else {
 		return nil, err
 	}
+	return nil, err
+}
+
+
+func (self *JWTValidator) VerifyClient(client string) bool {
+	return self.claims["client_id"] == client
+}
+
+
+func (self *JWTValidator) Validate(tokenString string) (jwt.MapClaims, error) {
+	claims, err := self.ParseJWT(tokenString)
+	if err != nil {
+		return nil, err
+	}
+	if ok := claims.VerifyIssuer(issuer, true); !ok {
+		return nil, errors.New("Unknown issuer.")
+	}
+	if ok := self.VerifyClient(appClientId); !ok {
+		return nil, errors.New("Unknown client.")
+	}
+	return claims, err
 }
