34d069b9cf65cdb9325722aafd2d53b840c5372d
MhmdRyhn <rayhan.cse12@gmail.com>
2020-11-11 03:37:47 +0600

    Remove old implementation of signup, confirm signuo, signin, refresh token auth, reset password, change password

--- service/auth/signup.go
+++ service/auth/signup.go
@@ -10,49 +10,6 @@ import (
 )
 
 
-// Users Signup by themselves
-func SignUp(username string, password string) (string, error) {
-	appClientId := os.Getenv("APP_CLIENT_ID")
-	newUserInput := &cognitoidp.SignUpInput {
-		ClientId: aws.String(appClientId),
-		Username: aws.String(username),
-		Password: aws.String(password),
-        UserAttributes: []*cognitoidp.AttributeType{
-            {
-                Name:  aws.String("email"),
-                Value: aws.String(username),
-			},
-		},
-	}
-
-	// client := cognitoidp.New(awsSession())
-	_, err := client.SignUp(newUserInput)
-	if err != nil {
-		return "", err
-	} else {
-		return fmt.Sprintf("User with email %s signed up successfully.", username), err
-	}
-}
-
-
-// Confirm Signup by providing a confirmation email sent to the user email
-func ConfirmSignUp(username string, confirmationCode string) (string, error) {
-	appClientId := os.Getenv("APP_CLIENT_ID")
-	confirmSignupInput := &cognitoidp.ConfirmSignUpInput {
-		ClientId: aws.String(appClientId),
-		Username: aws.String(username),
-		ConfirmationCode: aws.String(confirmationCode),
-	}
-
-	_, err := client.ConfirmSignUp(confirmSignupInput)
-	if err != nil {
-		return "", err
-	} else {
-		return fmt.Sprintf("User with email %s confirmed successfully.", username), err
-	}
-}
-
-
 // Admin creates user with a temporary password
 func AdminCreateUser(username string, temporaryPassword string) (string, error) {
 	userPoolId := os.Getenv("USER_POOL")
