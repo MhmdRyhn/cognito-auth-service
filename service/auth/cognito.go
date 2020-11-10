27dbd1e799020e491e2d7d471e31c9d44891ad80
MhmdRyhn <rayhan.cse12@gmail.com>
2020-11-11 04:13:08 +0600

    Remove old implementation of ResendConfirmationCode functionality

--- service/auth/cognito.go
+++ service/auth/cognito.go
@@ -2,7 +2,6 @@ package auth
 
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -37,24 +36,4 @@ var client *cognitoidp.CognitoIdentityProvider = cognitoidp.New(awsSession())
 // Extracts `error code` and `error message` from the `awserr`
 func CognitoErrorDetails(err error) (string, string) {
 	return err.(awserr.Error).Code(), err.(awserr.Error).Message()
-}
-
-
-// Resend a verification code to users' `email` to reset password
-func ResendConfirmationCode(username string) (string, error) {
-	appClientId := os.Getenv("APP_CLIENT_ID")
-
-	resendConfirmationCodeInput := &cognitoidp.ResendConfirmationCodeInput {
-		ClientId: aws.String(appClientId),
-		Username: aws.String(username),
-    }
-
-	_, err := client.ResendConfirmationCode(resendConfirmationCodeInput)
-	if err != nil {
-		return "", err
-	} else {
-		return fmt.Sprintf(
-			"A verification code will be resent to email %s if a user exists with this email.", username,
-		), err
-	}
 }
